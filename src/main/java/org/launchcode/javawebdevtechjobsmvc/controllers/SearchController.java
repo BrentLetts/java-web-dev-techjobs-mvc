336602604d411c36b0c7a55ce34f9190e3969b0a
Brent Letts <brent.letts@gmail.com>
2020-02-22 17:07:19 -0600

    Completed third and fourth TODO: Complete search functionality

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
+
 
 /**
  * Created by LaunchCode
@@ -17,12 +19,27 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 @RequestMapping("search")
 public class SearchController {
 
+    private static ArrayList<Job> jobs = new ArrayList<>();
+
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+//    @RequestMapping(value = "results{searchTerm}")
+    @PostMapping("results{searchTerm}")
+    public String displaySearchResults(String searchType, String searchTerm, Model model){
+        if(searchTerm.equals("")){
+            jobs = JobData.findAll();
+        }else{
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+        return "redirect:";
+    }
 
 }
