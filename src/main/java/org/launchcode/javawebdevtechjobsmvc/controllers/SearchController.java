17d580a24de66993795c58a40e11f87965bff3e5
Brent Letts <brent.letts@gmail.com>
2020-02-26 23:35:46 -0600

    Keeping the search fields at the top of the results page

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -2,6 +2,7 @@ package org.launchcode.javawebdevtechjobsmvc.controllers;
 
 import org.launchcode.javawebdevtechjobsmvc.models.Job;
 import org.launchcode.javawebdevtechjobsmvc.models.JobData;
+import org.launchcode.javawebdevtechjobsmvc.models.Search;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,24 +21,27 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 public class SearchController {
 
     private static ArrayList<Job> jobs = new ArrayList<>();
+//    private static Search searchObject = new Search();
 
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         model.addAttribute("jobs", jobs);
+//        model.addAttribute("search", new Search());
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    // TODO - Figure out the radio button settings
 //    @RequestMapping(value = "results{searchTerm}")
     @PostMapping("results{searchTerm}")
     public String displaySearchResults(String searchType, String searchTerm, Model model){
+        //String searchType = search.getType();
+
         if(searchTerm.equals("")){
             jobs = JobData.findAll();
-        }else{
+        }else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
-
         model.addAttribute("jobs", jobs);
         return "redirect:";
     }
---
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/models/Search.java
@@ -0,0 +1,14 @@
+package org.launchcode.javawebdevtechjobsmvc.models;
+
+public class Search {
+    private String type = "all";
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+}
